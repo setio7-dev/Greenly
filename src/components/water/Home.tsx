e6d901b874159144c32bd7c406ab191641f8ce85
Rayzen7 <nugrahasetio6@gmail.com>
2025-08-08 13:50:50 +0700

    update

--- src/components/water/Home.tsx
+++ src/components/water/Home.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex lg:flex-row flex-col-reverse justify-center items-center w-full mb-12 lg:px-0 px-6 h-[110vh] lg:h-screen mx-auto lg:gap-30 gap-10 lg:pt-10 pt-24'>
+    <div className='flex lg:flex-row flex-col-reverse justify-center items-center w-full mb-12 lg:px-0 px-6 h-[110vh] lg:h-screen mx-auto lg:gap-24 gap-10 lg:pt-10 pt-24'>
       <div className="lg:w-[600px] w-full flex flex-col gap-6" data-aos="fade-right" data-aos-duration="900">
         <h1 className='text-[#0097DC] font-[600] lg:text-[42px] text-[24px]'>Jaga Kualitasnya, Selamatkan Masa Depan</h1>
         <p className='lg:text-[16px] text-[14px] text-justify'>Nggak perlu jadi ilmuwan buat selamatin air bersih. Cukup mulai dari kebiasaan kecil: kurangi plastik, hemat air, dan jaga saluran pembuangan. Bayangkan, tiap tetes yang kamu hemat hari ini bisa jadi penyambung hidup untuk banyak orang esok hari. Temukan fakta mengejutkan, tips ramah lingkungan, dan aksi kecil yang berdampak besar. Yuk, rawat air bersama — karena perubahan besar selalu dimulai dari yang sederhana.</p>
